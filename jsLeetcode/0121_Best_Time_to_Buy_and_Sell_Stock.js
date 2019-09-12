bc10de39c5a180e60dceb815c053bffb67bbfbd8
Guan <33585352+GuanYangCLU@users.noreply.github.com>
2019-09-11 18:56:32 -0700

    0911_03

--- jsLeetcode/0121_Best_Time_to_Buy_and_Sell_Stock.js
+++ jsLeetcode/0121_Best_Time_to_Buy_and_Sell_Stock.js
@@ -1,3 +1,21 @@
+// Good
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let rs = 0;
+    if (prices.length < 2) return 0;
+    let minIndex = 0;
+    for (let i=1;i<prices.length;i++){
+        // console.log(maxHelper(i-1,prices));
+        if (prices[i] < prices[minIndex]) minIndex = i;
+        rs = Math.max(rs, prices[i] - prices[minIndex]);
+    }
+    return rs > 0 ? rs : 0;
+};
+
+// bad
 /**
  * @param {number[]} prices
  * @return {number}
